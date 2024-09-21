96fc57ab8f41e08ce37c4e3a656cdaae3dff49d9
satishkhair07 <164186445+satishkhair07@users.noreply.github.com>
2024-09-21 20:11:55 +0530

    Update eks-tools-setup.sh
    updated script to download latest version of eksctl

--- eks/eks-tools-setup.sh
+++ eks/eks-tools-setup.sh
@@ -3,21 +3,26 @@
 yum --help &>> /dev/null
 if [ $? -eq 0 ]
 then
-  yum install zip unzip -y
+  sudo yum install zip unzip -y
 else
-  apt update && apt install zip unzip -y
+  sudo apt update && apt install zip unzip -y
 fi
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
 
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
 
